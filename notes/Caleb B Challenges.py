87d4fe7c9c6293007d827771021e2024dc713c79
C-Ball13 <42385519+C-Ball13@users.noreply.github.com>
2018-12-10 09:44:22 -0800

    Challenges

--- notes/Caleb B Challenges.py
+++ notes/Caleb B Challenges.py
@@ -1,10 +1,28 @@
+# challenge1
+def challengel(first_name, last_name):
+
+    return last_name + ", " + first_name
 
 
-def challengel(firstname, lastname):
-    print(lastname, firstname)
+name1 = challengel("John", "Doe")
+print(name1)
 
-challengel("John", "Doe")
+# challenge2
+def challenge2(num):
+    factor = 0
+    product = 0
+    while factor < num + add + num + product == num:
+        factor = factor +1
+        prduct = factor * 2
+    if product == num:
+        return"$s is ever" % num
+    print(challenge2(3))
+    print()
+
+# challenge3
+b = int(input("Input the base:"))
+h = int(input("Input the height:"))
 
 
-
+area = b*h/2
 
