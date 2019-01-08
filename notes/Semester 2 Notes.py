eeb9aa16961df66f2db68c71ad50f9f13929d612
C-Ball13 <42385519+C-Ball13@users.noreply.github.com>
2019-01-08 09:44:59 -0800

    semester 2 Notes

---
+++ notes/Semester 2 Notes.py
@@ -0,0 +1,27 @@
+print('Hello World')
+
+# This is a singlr linr comment
+
+cars = 5
+driving = True
+
+print("I have %d cars" % 5)
+print("I have " + str(cars) + "cars")
+
+age = input("How old are you?")
+print(age + "??? You really shouldn't lie about your age")
+
+color = ("Red", "white", "black", "blue", "purple", "pink")
+print(len(color))
+color.append("Light blue")
+color.append("dark blue")
+
+
+print(color)
+color.pop("0")
+print(color)
+print(color[2])
+
+print(len(color))
+
+
