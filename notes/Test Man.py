7e0c4a17863754bc3884e043f6acf4776a886801
C-Ball13 <42385519+C-Ball13@users.noreply.github.com>
2019-01-24 09:43:28 -0800

    Test Man Game

--- notes/Test Man.py
+++ notes/Test Man.py
@@ -4,13 +4,13 @@ import sys
 
 # lets set some variables
 wordList = [
-    "Kendrick Lamar", "Landon Collins", "Jalen Ramsey", "JCole", "A Boogie", "EHS", "A.J. Bouye", "BoonDocks"
+    "kendrick lamar", "landon collins", "jalen ramsey", "jcole", "a boogie", "ehs", "a.j. bouye", "boondocks"
            ]
 
 guess_word = []
 secretWord = random.choice(wordList)
 length_word = len(secretWord)
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 letter_storage = []
 
 
@@ -41,16 +41,17 @@ def newfunc():
             sys.exit("That's a shame! Have a nice day")
         else:
             print("Please Answer only Yes or No")
-            continue
-    newFunc()
+        continue
 
 
 def change():
-    for character in secretWord: # printing blanks for each letter in secret word
+    for character in secretWord:  # printing blanks for each letter in secret word
         guess_word.append("-")
     print("Ok, so the word You need to guess has", length_word, "characters")
-    print("Be aware that You can enter only 1 letter from a-z\n\n")
-    print(guess_word)
+
+
+print("Be aware that You can enter only 1 letter from a-z\n\n")
+print(guess_word)
 
 
 def guessing():
@@ -65,7 +66,7 @@ def guessing():
 
             letter_storage.append(guess)
             if guess in secretWord:
-                print("You guessed correctly!")
+                print("You Got it!")
                 for x in range(0, length_word):
                     if secretWord[x] == guess:
                         guess_word[x] = guess
@@ -78,7 +79,7 @@ def guessing():
                 print("The letter is not in the word. Try Again!")
                 guess_taken += 1
                 if guess_taken == 10:
-                       print(" Sorry Mate, You lost :<! The secret word was",         secretWord)
+                    print(" Sorry Man, You lost :<! The secret word was",         secretWord)
 
 
 change()
