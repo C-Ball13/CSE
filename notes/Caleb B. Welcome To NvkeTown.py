17f67762988c7fc82bd49090064e49f9c16312e1
C-Ball13 <42385519+C-Ball13@users.noreply.github.com>
2019-02-08 10:26:44 -0800

    Notes

--- notes/Caleb B. Welcome To NvkeTown.py
+++ notes/Caleb B. Welcome To NvkeTown.py
@@ -1,18 +1,20 @@
 world_map = {
-    'Place Unknown Nvketown': {
-        'NAME': "Nvketown Blue House",
+    'Place Unknown Nuketown': {
+        'NAME': "Nuketown Blue House",
         'DESCRIPTION': "This is an abandon house but people come here to relax."
         "There are is an letter on the floor at the door.",
         'PATHS': {
             'WEST': "Looped_Road"
-        },
-        'LOOPED_ROAD': {
-            'NAME': "West_side of The Looped_Road",
-            'DESCRIPTION': "There is a blue car on your right and i suggest you not to the car.",
-            'PATHS': {
-                'EAST': "Place Unknown Nvketown"
-            }
         }
+    },
+    'LOOPED_ROAD': {
+        'NAME': "West_side of The Looped_Road",
+        'DESCRIPTION': "There is a blue car on your right and i suggest you not to the car.",
+        'PATHS': {
+            'EAST': "Place Unknown Nuketown"
+        }
+    },
+    'Yellow House': {
 
     }
 }
