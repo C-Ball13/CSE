148fa21ef2c7a9520fd872aff5649890464c8531
C-Ball13 <42385519+C-Ball13@users.noreply.github.com>
2019-03-15 00:05:58 -0700

    Object/NukeTown

--- notes/Caleb B. Object Project.py
+++ notes/Caleb B. Object Project.py
@@ -1,12 +1,45 @@
-class AN47 (object):
+class Gun(object):
     def __init__(self, clips_left):
         self.aim = True
-        self.in_clips = 30
+        self.in_clip = 30
         self.clips_left = clips_left
+        self.trigger = True
+        self.carrier = True
 
     def reload(self):
         if self.clips_left > 0:
-            self.in_clips = 30
+            self.in_clip = 30
             self.clips_left -= 1
 
-    def
+    def shoot(self, ammo):
+        if not self.carrier:
+            print("You don't have any ammo in the clip")
+            print("your trigger is jammed")
+            return
+        bullet_loss_per_shot = 3
+        self.clips_left -= ammo * bullet_loss_per_shot
+        if self.clips_left < 0:
+            self.clips_left = 0
+            print("There's no ammo in the clip")
+        elif self.clips_left == 0:
+            print("You ain't got anymore bullets after you killed him")
+        else:
+            print("You killed him")
+            print("You have this much ammo %d" % self.clips_left)
+
+    def throw_gun(self):
+        print("AHHHHHH!!!")
+        print("It had no more ammo")
+        self.carrier = False
+
+
+our_gun = Gun("AN47",)
+other_gun = Gun("TAC-2", )
+regular_gun = Gun("Mac_10")
+
+our_gun.shoot(3)
+our_gun.shoot(ammo)
+our_gun.clip_left(30)
+our_gun.shoot(5)
+other_gun.throw()
+other_gun.shot(3)
--- notes/Caleb B. Welcome To NvkeTown.py
+++ notes/Caleb B. Welcome To NvkeTown.py
@@ -1,111 +1,111 @@
 world_map = {
     'Place Unknown Nuketown': {
         'NAME': "Nuketown Blue House",
-        'DESCRIPTION': "This is an abandon house but people come here to relax."
-        "There are is an letter on the floor at the door.",
+        'DESCRIPTION':  "This is an abandon house but people come here to relax."
+                        "There are is an letter on the floor at the door.",
         'PATHS': {
             'WEST': "Looped_Road"
-        },
-        'LOOPED_ROAD': {
-            'NAME': "West_side of The Looped_Road",
-            'DESCRIPTION': "There is a blue car on your right and i suggest you not to the car.",
-            'PATHS': {
-                'EAST': "Nuketown Yellow House"
-            },
-            'Place_Unknown_Nuketown': {
-                'NAME': "Nuketown Yellow House",
-                'DESCRIPTION': "There's is a statue in the house as the door is wide open0",
-                'PATHS': {
-                    'EAST': "Yellow House Kitchen"
-                },
-                'Nuketown Yellow House': {
-                    'NAME': "Yellow House Kitchen",
-                    'DESCRIPTION': "There's a class cup on the counter and a sword"
-                    "The sword is glowing red and a lantern is on the floor.There's stairs going south",
-                    'PATHS': {
-                        'SOUTH': "Attic"
-                    },
-                    'Attic': {
-                        'NAME': "Yellow House Attic",
-                        'DESCRIPTION': "It's dark in here you should use the lantern",
-                        'PATHS': {
-                            'SOUTH': "Attic"
-                        },
-                        'Attic': {
-                            'NAME': "Attic",
-                            'DESCRIPTION': "This is an huge attic."
-                                           "Watch your step going down the stairs in here.",
-                            'PATHS': {
-                                'WEST': "Attic"
-                            },
-                            'Attic': {
-                                'NAME': "Attic Stairs",
-                                'DESCRIPTION': "You stepped on an broke step and fell through the steps "
-                                               "There's Keys on the floor"
-                                               "An thief is on the floor kill him before you get robbed.",
-                                'PATHS': {
-                                    'EAST': "East-side Attic"
-                                },
-                                'Attic': {
-                                    'NAME': "East-Side Attic",
-                                    'DESCRIPTION': "There's an sword glowing blue and a door",
-                                    'PATHS': {
-                                        'EAST': "Place Unknown Nuketown"
-                                    },
-                                    'Place Unknown Nuketown': {
-                                        'NAME': "Rest Room",
-                                        'DESCRIPTION': "There's and backpack on the floor"
-                                                       "Food and water on the back pack ",
-                                        'PATHS': {
-                                            'NORTH': "Place Unknown Nuketown"
-                                        },
-                                        'Place Unknown Nuketown': {
-                                            'NAME': "Attic Hallway",
-                                            'DESCRIPTION': "There's an room ahead with the same"
-                                                           "statue from the Yellow House",
-                                            'PATHS': {
-                                                'WEST': "Place Unknown Nuketown"
-                                            },
-                                            'PLace Unknown Nuketown': {
-                                                'NAME': "G-BO Game-Room",
-                                                'DESCRIPTION': "There's an rubber ducky on the floor and a phone",
-                                                'PATHS': {
-                                                    'WEST': "Place Unknown Nuketown"
-                                                },
-                                                'Place Unknown Nuketown': {
-                                                    'NAME': "West-Side Attic",
-                                                    'DESCRIPTION': "An witches broom is on the wall."
-                                                                   "There's and door ahead with lock on it",
-                                                    'PATHS': {
-                                                       'WEST': "Quad Studios"
-                                                    },
-                                                    'Place Unknown Nuketown': {
-                                                        'NAME': "Quad Studios",
-                                                        'DESCRIPTION': "The Roof comes off soon as you come in"
-                                                                       "An orc is on the wall look at you"
-                                                                       "you should protect yourself before you die",
-                                                        'PATHS': {
-                                                            "North": "Place Unknown Nuketown"
-                                                        },
-                                                        'Place Unknown Nuketown': {
-                                                            'NAME': "hp Afro Repairs",
-                                                            'DESCRIPTION': "Clouds sudden come out and you get iht "
-                                                                           "with Lightning you fall on an bed with"
-                                                                           "two other beds a bear is on an bed"
-                                                                           "you should talk to him"
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
         }
+    },
+    'LOOPED_ROAD': {
+        'NAME': "West_side of The Looped_Road",
+        'DESCRIPTION': "There is a blue car on your right and i suggest you not to the car.",
+        'PATHS': {
+            'EAST': "Nuketown Yellow House"
+            }
+    },
+    'Nuketown Yellow House': {
+        'NAME': "Nuketown Yellow House",
+        'DESCRIPTION': "There's is a statue in the house as the door is wide open0",
+        'PATHS': {
+            'EAST': "Yellow House Kitchen"
+        }
+    },
+    'Nuketown Yellow Kitchen': {
+        'NAME': "Yellow House Kitchen",
+        'DESCRIPTION':  "There's a class cup on the counter and a sword"
+                        "The sword is glowing red and a lantern is on the floor.There's stairs going south",
+        'PATHS': {
+            'SOUTH': "Attic"
+            }
+    },
+    'Attic': {
+        'NAME': "Yellow House Attic",
+        'DESCRIPTION': "It's dark in here you should use the lantern",
+        'PATHS': {
+            'SOUTH': "Watermelon Attic"
+            }
+    },
+    'Watermelon Attic': {
+        'NAME': "Attic",
+        'DESCRIPTION':  "This is an huge attic."
+                        "Watch your step going down the stairs in here.",
+        'PATHS': {
+            'WEST': "Attic Stairs"
+            }
+    },
+    'Attic Stairs': {
+        'NAME': "Attic Stairs",
+        'DESCRIPTION':  "You stepped on an broke step and fell through the steps "
+                        "There's Keys on the floor"
+                        "An thief is on the floor kill him before you get robbed.",
+        'PATHS': {
+            'EAST': "East-side Attic"
+             },
+    },
+    'East-Side attic': {
+        'NAME': "East-Side Attic",
+        'DESCRIPTION': "There's an sword glowing blue and a door",
+        'PATHS': {
+            'EAST': "Place Unknown Nuketown"
+            }
+    },
+    'Rest-Room': {
+        'NAME': "Rest Room",
+        'DESCRIPTION':  "There's and backpack on the floor"
+                        "Food and water on the back pack ",
+        'PATHS': {
+            'NORTH': "Attic Hallway"
+            }
+    },
+    'Attic Hallway': {
+        'NAME': "Hallway",
+        'DESCRIPTION':  "There's an room ahead with the same"
+                        "statue from the Yellow House",
+        'PATHS': {
+            'WEST': "G-BO Game-Room"
+        }
+    },
+    'G-BO Game-Room': {
+        'NAME': "G-BO Game-Room",
+        'DESCRIPTION': "There's an rubber ducky on the floor and a phone",
+        'PATHS': {
+            'WEST': "West-Side Attic"
+        }
+    },
+    'West-Side Attic': {
+        'NAME': "West-Side Attic",
+        'DESCRIPTION':  "An witches broom is on the wall."
+                        "There's and door ahead with lock on it",
+        'PATHS': {
+            'WEST': "Quad Studios"
+            }
+    },
+    'Quad Studios': {
+        'NAME': "Quad Studios",
+        'DESCRIPTION':  "The Roof comes off soon as you come in"
+                        "An orc is on the wall look at you"
+                        "you should protect yourself before you die",
+        'PATHS': {
+            "North": "hp Afro Repair"
+            }
+    },
+    'hp Afro Repair': {
+        'NAME': "hp Afro Repair",
+        'DESCRIPTION':  "Clouds sudden come out and you get iht "
+                        "with Lightning you fall on an bed with"
+                        "two other beds a bear is on an bed"
+                        "you should talk to him"
     }
 }
 
