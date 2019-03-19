83938c550a02dad2270ed5b60fc7527cf1fa2a01
C-Ball13 <42385519+C-Ball13@users.noreply.github.com>
2019-03-19 09:45:09 -0700

    items

--- notes/Caleb B. Items.py
+++ notes/Caleb B. Items.py
@@ -3,3 +3,13 @@ class Item(object):
         self.name = name
 
 
+class Weapon(Item):
+    def __init__(self, name, damage):
+        super(Weapon, self).__init__(name)
+        self.damage = damage
+
+
+class Armor(Item):
+    def __init__(self, name, armor_ant):
+        super(Armor, self).__init__(name)
+        self.armor_ant = armor_ant
