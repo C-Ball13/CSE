12f27b4d750edee17b1ced48235539a171716b44
C-Ball13 <42385519+C-Ball13@users.noreply.github.com>
2019-04-25 09:45:09 -0700

    csv

--- notes/Welcome To Nuketown Pt2.py
+++ notes/Welcome To Nuketown Pt2.py
@@ -23,12 +23,37 @@ class Player(object):
         """
         self.current_location = new_location
 
-    def find_next_room
-        """This method searches the current room so see if a room
+    def find_next_room(self, directions):
+    """This method searches the current room so see if a room
                exists in that direction.
 
         :param direction: The direction that you want to move to
         :return: The Room object if it exists, or None if it does not
         """
-        name_of_room = getattr(self.current_location, direction)
-        return globals(name_of_room)
+    name_of_room = getattr(self.current_location, directions)
+    return globals()[name_of_room]
+
+
+
+Place_Unknown_Nuketown = Room("Nuketown Blue House", "This is where you are now", 'Looped_Road')
+Looped_Road = Room("WestSide of looped Road", "", None, "Place Unknown Nuketown")
+
+player = Player(Place_Unknown_Nuketown)
+
+playing = True
+directions = ['north', 'south', 'east', 'west', 'up', 'down']
+
+while playing:
+    print(player.current_location.name)
+    print(player.current_location.description)
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command.lower() in directions:
+        try:
+            next_room = player.find_next_room(command)
+            player.move(next_room)
+        except KeyError:
+            print("I can't go that way")
+        else:
+            print("Command Not Found")
