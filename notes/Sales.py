56564c28ff94537a37b784e6e6a560d48b040752
C-Ball13 <42385519+C-Ball13@users.noreply.github.com>
2019-05-08 09:47:06 -0700

    Salesa

---
+++ notes/Sales.py
@@ -0,0 +1,7 @@
+import csv
+
+with open("Sakes Records.csv") as old_csv:
+    with open("MyNewFile.csv", "w", newline='') as new_csv:
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
+        print("Processing.....")
