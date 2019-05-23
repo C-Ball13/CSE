0332c9ab0d0da4ef0ca0bde48962c60d7e835570
C-Ball13 <42385519+C-Ball13@users.noreply.github.com>
2019-05-22 22:01:23 -0700

    Update Welcome To Nuketown Pt2.py

--- notes/Welcome To Nuketown Pt2.py
+++ notes/Welcome To Nuketown Pt2.py
@@ -37,23 +37,38 @@ class Player(object):
 
 Place_Unknown_Nuketown = Room("Nuketown Blue House", "This is where you are now ", None, None, "Looped_Road", None,
                               None, None)
-Looped_Road = Room("WestSide of looped Road", "There's and letter on the floor ", None, None, "Nuketown_Yellow_House",
-                   "Place_Unknown_Nuketown", None, None)
-Nuketown_Yellow_House = Room("Nuketown Yellow House", "There's items this house. But there an orc in the kitchen.",
+Looped_Road = Room("WestSide of looped Road", "There's an letter on the floor and helmet next to it", None, None,
+                   "Nuketown_Yellow_House", "Place_Unknown_Nuketown", None, None)
+Nuketown_Yellow_House = Room("Nuketown Yellow House", "there's an chest plate on the couch and dead body next to it.",
                              None, None, "Nuketown_Kitchen", "Looped_Road", None, None)
-Nuketown_Kitchen = Room("Yellow House Kitchen", "", None, None, None, None, "Nuketown_Yellow_House", "Attic")
-Attic = Room("Yellow House Attic", "", None, None, None, None, "Nuketown_Kitchen", "Watermelon_Attic")
-Watermelon_Attic = Room("Attic", "", None, None, None, None, "Attic", "Attic_Stairs")
-Attic_Stairs = Room("Attic Stairs", "", None, None, "East_Side_attic", "Watermelon_Attic", None, None)
-East_Side_attic = Room("East-Side Attic", "", None, None, None, None, "East_Side_attic", "Rest_Room")
-Rest_Room = Room("Rest Room", "", "Attic_Hallway", None, None, None, "East_Side_attic", None)
-Attic_Hallway = Room("Hallway", "", None, "Rest_Room", None, "GBO_Game_Room", None, None)
-GBO_Game_Room = Room("G-BO Game-Room", "", None, None, "Attic_Hallway", "West_Side_Attic", None, None)
-West_Side_Attic = Room("West-Side Attic", "", None, None, "GBO_Game_Room", "Quad_Studios", None, None)
-Quad_Studios = Room("Quad Studios", "", None, "hp_Afro_Repair", "West_Side_Attic", None)
-hp_Afro_Repair = Room("hp Afro Repair", "", "Quad_Studios", None, "Cafe", None, None, None)
-Cafe = Room("Cafeteria", "", None, None, None, "hp_Afro_Repair", None, None)
+Nuketown_Kitchen = Room("Yellow House Kitchen", "There's an orc eating food and in your vision there's an gun XR2",
+                        None,None, None, None, "Nuketown_Yellow_House", "Attic")
+Attic = Room("Yellow House Attic", "there's two orc in here ready to kill you better think fast. ", None, None, None,
+             None, "Nuketown_Kitchen", "Watermelon_Attic")
+Watermelon_Attic = Room("Attic", "this is an bigger attic and there some weapon and leggings as armor ", None, None, None, None, "Attic", "Attic_Stairs")
+Attic_Stairs = Room("Attic Stairs", "there's an orc on theses stairs kill him now", None, None, "East_Side_attic",
+                    "Watermelon_Attic", None, None)
+East_Side_attic = Room("East-Side Attic", "There some boots on the floor and back plate and "
+                                          "be careful orc be popping out", None, None, None, None, "East_Side_attic",
+                       "Rest_Room")
+Rest_Room = Room("Rest Room", "there's some wrists guards that boost your health ", "Attic_Hallway", None, None, None,
+                 "East_Side_attic", None)
+Attic_Hallway = Room("Attic_Hallway", "there's an orc that look bigger than all the other one's "
+                                      "this should take longer than the other ones", None, "Rest_Room",
+                     None, "GBO_Game_Room", None, None)
+GBO_Game_Room = Room("G-BO Game-Room", "there's Longbow,Polearm,Spears,and Sword",
+                     None, None, "Attic_Hallway", "West_Side_Attic", None, None)
+West_Side_Attic = Room("West-Side Attic", "an light on the wall started to glow and it says an enemy is near",
+                       None, None, "GBO_Game_Room", "Quad_Studios", None, None)
+Quad_Studios = Room("Quad Studios", "there's clippers on the floor and 2 orcs in here", None, "hp_Afro_Repair",
+                    "West_Side_Attic", None)
+hp_Afro_Repair = Room("hp Afro Repair", "an light turn on and says grab 2 thing only. There's MarksmanRifle,"
+                                        "AssaultShotgun,Axe,and SMG", "Quad_Studios", None, "Cafe", None, None, None)
+Cafe = Room("Cafeteria", "you should put and necklaces and some shoulder blades on "
+                         "that's on the wall because there's an boos you have to kill ",
+            None, None, None, "hp_Afro_Repair", None, None)
 player = Player(Place_Unknown_Nuketown)
+
 
 playing = True
 directions = ['north', 'south', 'east', 'west', 'up', 'down']
@@ -96,3 +111,4 @@ while playing:
             print("I can't go that way")
     else:
         print("Command Not Found")
+
